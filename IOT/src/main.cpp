35b3a47212ddfddd116fb2cd95477e3655ae2da1
LotusZaheer <andresfelipeuribe11@gmail.com>
2022-01-19 23:40:36 -0500

    Pruebas datos falsos

--- IOT/src/main.cpp
+++ IOT/src/main.cpp
@@ -1,68 +1,82 @@
 #include <Arduino.h>
 #include <Colors.h>
 #include <IoTicosSplitter.h>
-#include <ESP8266WiFi.h>       //#include <WiFi.h>
+#include <ESP8266WiFi.h>	   //#include <WiFi.h>
 #include <ESP8266HTTPClient.h> //<HTTPClient.h>
 #include <ArduinoJson.h>
 #include <PubSubClient.h>
+#include <stdlib.h>
 
 //----------- MQTT CONFIG -----------//
-const char *mqtt_server = "ioticos.org";
+const char *mqtt_server = "flowriver.online";
 const int mqtt_port = 1883;
-const char *mqtt_user = "";
-const char *mqtt_pass = "";
+const char *mqtt_user = "ESP8266-1";
+const char *mqtt_pass = "flowriver";
 const char *root_topic_subscribe = "testtopic";
-const char *root_topic_publish = "testtopic";
-
+const char *root_topic_publish = "flowriver/Esp8266-1/datos";
 
 //----------- WIFI -----------//
-const char* ssid = "Callen";
-const char* password =  "algoalgo";
-
-
+const char *ssid = "CASAUIS";
+const char *password = "a1b2c3d4/casauis";
 
 //----------- Globales -----------//
 WiFiClient espClient;
 PubSubClient client(espClient);
-char msg[25];
-long count=0;
+char msg[125];
+long NivelAgua = 0;
+long VelocidadAgua = 0;
+long Temperatura = 0;
+long Humedad = 0;
+long Precipitaciones = 0;
 
 
 //----------- Funciones -----------//
-void callback(char* topic, byte* payload, unsigned int length);
+void callback(char *topic, byte *payload, unsigned int length);
 void reconnect();
 void setup_wifi();
 
-
 void setup()
 {
-  Serial.begin(921600);
-  setup_wifi();
-  client.setServer(mqtt_server, mqtt_port);
-  client.setCallback(callback);
+	Serial.begin(921600);
+	setup_wifi();
+	client.setServer(mqtt_server, mqtt_port);
+	client.setCallback(callback);
 }
 
-void loop() {
-  
-  count++;
+void loop()
+{
 
-  if (!client.connected()) {
+	if (!client.connected())
+	{
 		reconnect();
 	}
+	
+	  if (client.connected()){
+		NivelAgua = 1+rand()%(5-1);
+		VelocidadAgua = 45+rand()%(65-45);
+		Temperatura =19+rand()%(29-19);
+		Humedad =20+rand()%(100-20);
+		Precipitaciones = 0+rand()%(10-0);
+		String str = "{'NivelAgua':" + String(NivelAgua)
+		+ ",'VelocidadAgua':" + String(VelocidadAgua) 
+		+ ",'Temperatura':" + String(Temperatura)
+		+ ",'Humedad':" + String(Humedad)
+		+ ",'Precipitaciones':" + String(Precipitaciones) + "}";
+		str.toCharArray(msg,125);
+		client.publish(root_topic_publish,msg);
+		delay(60000);
+	  }
 
-  if (client.connected()){
-    String str = "La cuenta es -> " + String(count);
-    str.toCharArray(msg,25);
-    client.publish(root_topic_publish,msg);
-    delay(300);
-  }
-  client.loop();
+	client.loop();
 }
 
 //----------- Conexión Wifi -----------//
 
-void setup_wifi(){
+void setup_wifi()
+{
+
 	delay(10);
+
 	// Nos conectamos a nuestra red Wifi
 	Serial.println();
 	Serial.print("Conectando a ssid: ");
@@ -70,7 +84,8 @@ void setup_wifi(){
 
 	WiFi.begin(ssid, password);
 
-	while (WiFi.status() != WL_CONNECTED) {
+	while (WiFi.status() != WL_CONNECTED)
+	{
 		delay(500);
 		Serial.print(".");
 	}
@@ -81,26 +96,33 @@ void setup_wifi(){
 	Serial.println(WiFi.localIP());
 }
 
-
 //----------- Conexión MQTT -----------//
 
-void reconnect() {
+void reconnect()
+{
 
-	while (!client.connected()) {
+	while (!client.connected())
+	{
 		Serial.print("Intentando conexión Mqtt...");
 		// Creamos un cliente ID
 		String clientId = "IOTICOS_H_W_";
 		clientId += String(random(0xffff), HEX);
 		// Intentamos conectar
-		if (client.connect(clientId.c_str(),mqtt_user,mqtt_pass)) {
+		if (client.connect(clientId.c_str(), mqtt_user, mqtt_pass))
+		{
 			Serial.println("Conectado!");
 			// Nos suscribimos
-			if(client.subscribe(root_topic_subscribe)){
-        Serial.println("Suscripcion ok");
-      }else{
-        Serial.println("fallo Suscripciión");
-      }
-		} else {
+			if (client.subscribe(root_topic_subscribe))
+			{
+				Serial.println("Suscripcion ok");
+			}
+			else
+			{
+				Serial.println("fallo Suscripciión");
+			}
+		}
+		else
+		{
 			Serial.print("falló :( con error -> ");
 			Serial.print(client.state());
 			Serial.println(" Intentamos de nuevo en 5 segundos");
@@ -111,15 +133,16 @@ void reconnect() {
 
 //----------- Callback -----------//
 
-void callback(char* topic, byte* payload, unsigned int length){
+void callback(char *topic, byte *payload, unsigned int length)
+{
 	String incoming = "";
-	Serial.print("Mensaje recibido desde -> ");
+	Serial.print("Desde -> ");
 	Serial.print(topic);
 	Serial.println("");
-	for (int i = 0; i < length; i++) {
+	for (int i = 0; i < length; i++)
+	{
 		incoming += (char)payload[i];
 	}
 	incoming.trim();
-	Serial.println("Mensaje -> " + incoming);
-
+	Serial.println("-> " + incoming);
 }
